16ad344ee1a4054a55b539f7108faa9c61ab1bc6
PraveenrajKumar529 <praveenrajkumar062@gmail.com>
2025-11-11 17:04:58 +0530

    insert.py

---
+++ python/class-20-ERROR HANDLINGS/one.py
@@ -0,0 +1,10 @@
+print(10/5)
+try:
+    print(10/0)
+
+except:
+    print("cant divisible by zero")
+    
+print(10/1)
+print("ge")
+#program terminate abnormally
---
+++ python/class-20-ERROR HANDLINGS/two.py
@@ -0,0 +1,4 @@
+a=int(input("ENTER FN"))
+b=int(input("enter & n"))
+print(aCCCCCC)
+try:\a=imy=input9: (moofird)
